f404bc6a6a9787f2b8d506b8e0dbd17a97925a88
Sipu Kumar <sipuk1896@gmail.com>
2026-04-10 10:35:57 +0530

    new feature

--- app.py
+++ app.py
@@ -1,10 +1,21 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "<h1>Welcome to My Portfolio 🚀</h1><p>This is my Flask app</p>"
+    return """
+    <html>
+    <head>
+        <title>Portfolio</title>
+        <link rel="stylesheet" href="/static/style.css">
+    </head>
+    <body>
+        <h1>Welcome to My Portfolio 🚀</h1>
+        <p>This is my updated UI</p>
+    </body>
+    </html>
+    """
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8000)
